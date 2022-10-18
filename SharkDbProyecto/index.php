29c58a5dc3673e5177edb20294672aaf68340612
Julián <87765288+artebleker@users.noreply.github.com>
2022-10-18 18:31:53 -0300

    cambios en Index
    video
    nuestro espacio
    nuestros entrenadores

--- SharkDbProyecto/index.php
+++ SharkDbProyecto/index.php
@@ -1,12 +1,27 @@
 <?php include("template/header.php");?>
 
         <div class="jumbotron">
-            <h1 class="display-3">Bienvenido/a</h1>
-            <p class="lead">TPO Sharkdb.</p>
-            <hr class="my-2">
-            <p class="lead">
-                <a class="btn btn-primary btn-lg" href="Design.php" role="button">Ingresar al listado de diseños</a>
-            </p>
+            <!-- VIDEO inicio-->
+            <div class="video-index">
+                <iframe src="https://www.youtube.com/embed/8IEjWNJzoNo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <div class="title-index">      
+                    <h1 class="display-3 text-center">Potential Stength</h1>
+        </div>
+        </div>
+        <!-- Nuestro espacio -->
+        <section class="section-index">
+            <h1 class="fs-1 text-center">Nuestro espacio</h1>
+            <div class="title-section-index">
+                <img src="./img/indexgym.jpg" alt="Espacio de gimnasio"/>
+            </div>
+        </section>
+        <!-- Nuestros entrenadores -->
+        <section class="section-index">
+            <h1 class="fs-1 text-center">Nuestros entrenadores</h1>
+            <div class="title-section-index">
+                <img src="./img/entrenadores.jpg" alt="Entrenadores"/>
+            </div>
+        </section>
         </div>
         
 <?php include("template/footer.php");?>
