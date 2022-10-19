efa20a6e6aa563cffe0fe230d3885a2bf5fa2faf
Elias <moriancumer6@gmail.com>
2022-10-18 23:29:41 -0300

    rutinas y perfil

--- SharkDbProyecto/usuario/perfil.php
+++ SharkDbProyecto/usuario/perfil.php
@@ -1,12 +1,17 @@
 <?php include('template/header.php') ?>
-<div class="text-center col-md-12 usuario-container">
-    <div class="jumbotron usuario-transparencia">        
-        <h1>Mi Perfil</h1>            
-        <div class="mt-5">
-            <p>Toda tu informacion en un solo lugar!!</p>
-            <p class="mt-5">
-                <a class="btn btn-outline-primary btn-lg" href="inicio.php" role="button">Volver</a>                
-            </p>
+<div class=" col-md-12 usuario-container">
+    <div class="jumbotron usuario-transparencia row">        
+        <h1 class="text-white col-12 mb-5">Mi Perfil</h1>            
+        <div class="col-6">
+            <h5><ion-icon name="person-outline"></ion-icon> Nombre y Apellido</h5>
+            <p></p>
+            <p><ion-icon name="mail-outline"></ion-icon> Email</p>
+            <p><ion-icon name="accessibility-outline"></ion-icon> Apto fisico</p>
+        </div>
+        <div class="col-6">
+            <p><ion-icon name="barbell-outline"></ion-icon> Peso</p>
+            <p></ion-icon><ion-icon name="man-outline"></ion-icon> Altura</p>
+            <p><ion-icon name="card-outline"></ion-icon> DNI</p>
         </div>
     </div>
 </div>
@@ -19,3 +24,6 @@
         body.classList.add("usuario-perfil");
     }, false);
 </script>
+
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
--- SharkDbProyecto/usuario/rutinas.php
+++ SharkDbProyecto/usuario/rutinas.php
@@ -1,4 +1,5 @@
 <?php include('template/header.php') ?>
+<!--
 <div class="text-center col-md-12 usuario-container">
     <div class="jumbotron usuario-transparencia">        
         <h1>Mis Rutinas</h1>            
@@ -10,12 +11,49 @@
         </div>
     </div>
 </div>
+-->
+    <div class="col-md-4">
+        <div class="card usuario-transparencia" style="width: 18rem;">
+        <iframe width="285" height="315" src="https://www.youtube.com/embed/xYLfVvC8dD8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <div class="card-body">
+                <h5 class="card-title"><ion-icon name="barbell-outline"></ion-icon> Nombre del ejercicio</h5>
+                <p class="card-text"><ion-icon name="walk-outline"></ion-icon> Fecha de inicio: 18-10-22.</p>
+                <p class="card-text"><ion-icon name="time-outline"></ion-icon> Duracion: 3 semanas.</p>
+                <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="card usuario-transparencia" style="width: 18rem;">
+        <iframe width="285" height="315" src="https://www.youtube.com/embed/G41I95bBaHM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <div class="card-body">
+                <h5 class="card-title"><ion-icon name="barbell-outline"></ion-icon> Nombre del ejercicio</h5>
+                <p class="card-text"><ion-icon name="walk-outline"></ion-icon> Fecha de inicio: 18-10-22.</p>
+                <p class="card-text"><ion-icon name="time-outline"></ion-icon> Duracion: 3 semanas.</p>
+                <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="card usuario-transparencia" style="width: 18rem;">
+        <iframe width="285" height="315" src="https://www.youtube.com/embed/TYWEHD11bnM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <div class="card-body">
+                <h5 class="card-title"><ion-icon name="barbell-outline"></ion-icon> Nombre del ejercicio</h5>
+                <p class="card-text"><ion-icon name="walk-outline"></ion-icon> Fecha de inicio: 18-10-22.</p>
+                <p class="card-text"><ion-icon name="time-outline"></ion-icon> Duracion: 3 semanas.</p>
+                <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+            </div>
+        </div>
+    </div>
 <?php include('template/footer.php') ?>
 
 <script>
-    document.addEventListener('DOMContentLoaded', function () {                  
+    document.addEventListener('DOMContentLoaded', function() {
         let body = document.getElementById('usuarioBody');
         body.classList.remove("usuario-fondo");
         body.classList.add("usuario-rutina");
     }, false);
 </script>
+
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
